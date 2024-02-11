0cb992967e2332090c01faae0f518978b8d0fa21
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-02-11 22:00:15 +0530

    Time: 624 ms (41.94%), Space: 67.8 MB (9.86%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.py
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -1,21 +1,12 @@
 import pandas as pd
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    # Ensure the 'recordDate' column is a datetime type
-    weather['recordDate'] = pd.to_datetime(weather['recordDate'])
-    
-    # Sorting the DataFrame by 'recordDate' to ensure the shift operation works correctly
-    weather.sort_values('recordDate', inplace=True)
-    
-    # Creating new columns for the previous day's temperature and record date
-    weather['PreviousTemperature'] = weather['temperature'].shift(1)
-    weather['PreviousRecordDate'] = weather['recordDate'].shift(1)
-    
-    # Filtering the DataFrame to find rows where the temperature is higher 
-    # than the previous day and the date is exactly one day more than the previous record date
-    result = weather[
-        (weather['temperature'] > weather['PreviousTemperature']) & 
-        (weather['recordDate'] == weather['PreviousRecordDate'] + pd.Timedelta(days=1))
-    ][['id']].rename(columns={'id': 'Id'})
-    
-    return result
+    weather["recordDate"] = pd.to_datetime(weather["recordDate"])
+    weather.sort_values("recordDate", inplace = True)
+    weather["lastDate"] = weather["recordDate"].shift(1)
+    weather["lastTemp"] = weather["temperature"].shift(1)
+    df = weather[
+        (weather["temperature"]>weather["lastTemp"] ) & 
+        (weather["recordDate"] == weather["lastDate"] + pd.Timedelta(days=1))
+    ]
+    return df[["id"]]
