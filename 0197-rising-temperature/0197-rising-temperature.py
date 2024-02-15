216ae7131fd6d21e599fc3fb21381efa8645757e
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-02-15 17:04:40 +0530

    Time: 524 ms (82.16%), Space: 67.7 MB (15.11%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.py
+++ 0197-rising-temperature/0197-rising-temperature.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    weather["recordDate"] = pd.to_datetime(weather["recordDate"])
-    weather.sort_values("recordDate", inplace = True)
+    weather["recordDate"]=pd.to_datetime(weather["recordDate"])
+    weather.sort_values("recordDate", inplace= True)
     weather["lastDate"] = weather["recordDate"].shift(1)
     weather["lastTemp"] = weather["temperature"].shift(1)
-    df = weather[
-        (weather["temperature"]>weather["lastTemp"] ) & 
-        (weather["recordDate"] == weather["lastDate"] + pd.Timedelta(days=1))
-    ]
+    df = weather[(weather["temperature"] > weather["lastTemp"]) & (weather["recordDate"] == weather["lastDate"] + pd.Timedelta(days=1))]
     return df[["id"]]
