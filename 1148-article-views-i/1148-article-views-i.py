44abad8d6bd44f518c05aae14f19c5ec64d35b7f
Mohd Rizwan <141806496+Rizwans-github@users.noreply.github.com>
2024-07-28 23:42:36 +0530

    Time: 355 ms (92.58%), Space: 70.5 MB (5.52%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.py
+++ 1148-article-views-i/1148-article-views-i.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 def article_views(views: pd.DataFrame) -> pd.DataFrame:
-    df = views[views["author_id"] == views["viewer_id"]]
-    df = df[["author_id"]]
-    df = df.drop_duplicates()
-    df = df.sort_values("author_id", ascending = True)
-    df = df.rename(columns = {"author_id": "id"})
-    return df
+    df = views[views['author_id'] == views['viewer_id']]
+    df = df.drop_duplicates('author_id')
+    df = df.sort_values('author_id', ascending = True )
+    df['id'] = df['author_id']
+    return df[['id']]
